a5c46ba2b220cfde6e7c8b52e3abeeae798e7617
Atharva Biyani <atharvabiyani@gmail.com>
2025-04-08 11:26:49 -0500

    Final updates to portfolio before deployment

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,6 @@ function Footer() {
           <h3>Designed by Atharva Biyani</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
